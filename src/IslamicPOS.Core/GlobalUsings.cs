23d13837719e1facbe91cc575749fb5a38deb32b
Zubeid Hendricks <zubeid.hendricks@gmail.com>
2024-12-08 23:02:35 +0200

    Add global usings for common namespaces

---
+++ src/IslamicPOS.Core/GlobalUsings.cs
@@ -0,0 +1,14 @@
+global using System;
+global using System.Collections.Generic;
+global using System.Threading.Tasks;
+global using Microsoft.Extensions.Configuration;
+global using Microsoft.Extensions.DependencyInjection;
+global using Microsoft.Extensions.Logging;
+
+global using IslamicPOS.Core.Models.Common;
+global using IslamicPOS.Core.Models.Products;
+global using IslamicPOS.Core.Models.Transactions;
+global using IslamicPOS.Core.Models.Auth;
+global using IslamicPOS.Core.Models.Logistics;
+global using IslamicPOS.Core.Models.Reports;
+global using IslamicPOS.Core.Models.IslamicFinance;
