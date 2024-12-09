e0883745f5d59e2cb916bc9e1921c0858d3fa6ad
Zubeid Hendricks <zubeid.hendricks@gmail.com>
2024-12-10 00:56:44 +0200

    Add application startup configuration and web files

--- src/IslamicPOS.Web/Program.cs
+++ src/IslamicPOS.Web/Program.cs
@@ -1,12 +1,20 @@
 using IslamicPOS.Application;
 using IslamicPOS.Infrastructure;
+using IslamicPOS.Infrastructure.Persistence;
+using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<App>(".app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddApplication();
 builder.Services.AddInfrastructure(builder.Configuration);
 
 await builder.Build().RunAsync();
