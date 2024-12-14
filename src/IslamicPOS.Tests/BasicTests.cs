611e34798cecb7cfd454207cad21ad737659d2f9
Zubeid Hendricks <zubeid.hendricks@gmail.com>
2024-12-14 08:29:31 +0200

    Add Tests project with basic test

---
+++ src/IslamicPOS.Tests/BasicTests.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace IslamicPOS.Tests
+{
+    [TestClass]
+    public class BasicTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
