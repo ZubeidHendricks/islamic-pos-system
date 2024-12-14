39e361ce60e5d47b2fd3ed3bb7807d92b029d6de
Zubeid Hendricks <zubeid.hendricks@gmail.com>
2024-12-14 15:22:17 +0200

    Clean up global usings

--- src/IslamicPOS.Core/GlobalUsings.cs
+++ src/IslamicPOS.Core/GlobalUsings.cs
@@ -8,15 +8,3 @@ global using System.Threading.Tasks;
 global using Microsoft.Extensions.Configuration;
 global using Microsoft.Extensions.DependencyInjection;
 global using Microsoft.Extensions.Logging;
-
-// Core domain models
-global using IslamicPOS.Core.Models.Common;
-global using IslamicPOS.Core.Models.Product;
-global using IslamicPOS.Core.Models.Transaction;
-global using IslamicPOS.Core.Models.Auth;
-global using IslamicPOS.Core.Models.Logistics;
-global using IslamicPOS.Core.Models.IslamicFinance;
-
-// Core services and interfaces
-global using IslamicPOS.Core.Services;
-global using IslamicPOS.Core.Interfaces;
