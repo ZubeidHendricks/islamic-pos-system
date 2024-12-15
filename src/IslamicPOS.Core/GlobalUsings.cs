bef8ca228efd4c33bf33bbe9cfac6745eb4b1d76
Zubeid Hendricks <zubeid.hendricks@gmail.com>
2024-12-15 12:23:44 +0200

    Setup clean Core project structure

--- src/IslamicPOS.Core/GlobalUsings.cs
+++ src/IslamicPOS.Core/GlobalUsings.cs
@@ -1,10 +1,13 @@
+// System namespaces
 global using System;
 global using System.Collections.Generic;
 global using System.Linq;
 global using System.Threading.Tasks;
 
+// Microsoft extensions
 global using Microsoft.Extensions.Configuration;
 global using Microsoft.Extensions.DependencyInjection;
 global using Microsoft.Extensions.Logging;
 
+// Project namespaces
 global using IslamicPOS.Core.Common;
