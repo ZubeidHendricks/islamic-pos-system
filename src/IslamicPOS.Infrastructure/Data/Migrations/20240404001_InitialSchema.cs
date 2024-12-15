81aa4e5af6a63f0c0f42fdadada47c0a239da57a
Zubeid Hendricks <zubeid.hendricks@gmail.com>
2024-12-15 23:35:06 +0200

    Update base migration classes to forward to partial methods

--- src/IslamicPOS.Infrastructure/Data/Migrations/20240404001_InitialSchema.cs
+++ src/IslamicPOS.Infrastructure/Data/Migrations/20240404001_InitialSchema.cs
@@ -1,9 +1,19 @@
-using System;
 using Microsoft.EntityFrameworkCore.Migrations;
 
 namespace IslamicPOS.Infrastructure.Data.Migrations;
 
-public partial class InitialSchema
+public partial class InitialSchema : Migration
 {
-    // Intentionally left empty to resolve duplicate method issue
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        UpInitialSchema(migrationBuilder);
+    }
+
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        DownInitialSchema(migrationBuilder);
+    }
+
+    partial void UpInitialSchema(MigrationBuilder migrationBuilder);
+    partial void DownInitialSchema(MigrationBuilder migrationBuilder);
 }
--- src/IslamicPOS.Infrastructure/Data/Migrations/20241208001_InitialCreate.cs
+++ src/IslamicPOS.Infrastructure/Data/Migrations/20241208001_InitialCreate.cs
@@ -1,9 +1,19 @@
-using System;
 using Microsoft.EntityFrameworkCore.Migrations;
 
 namespace IslamicPOS.Infrastructure.Data.Migrations;
 
-public partial class InitialCreate
+public partial class InitialCreate : Migration
 {
-    // Intentionally left empty to resolve duplicate method issue
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        UpInitialCreate(migrationBuilder);
+    }
+
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        DownInitialCreate(migrationBuilder);
+    }
+
+    partial void UpInitialCreate(MigrationBuilder migrationBuilder);
+    partial void DownInitialCreate(MigrationBuilder migrationBuilder);
 }
