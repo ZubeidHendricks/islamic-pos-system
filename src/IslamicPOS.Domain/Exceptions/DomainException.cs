cadd00859f3348511ff0e0aafdad20ce0eb8810f
ZubeidHendricks <zubeid.hendricks@gmail.com>
2024-12-16 00:39:15 +0200

    mcp:DEVX: Fix DomainException validation errors
    - Add required modifier to ErrorCode property
    - Add Obsolete attributes to serialization members
    - Initialize ErrorCode in all constructors
    - Fix nullable warnings

--- src/IslamicPOS.Domain/Exceptions/DomainException.cs
+++ src/IslamicPOS.Domain/Exceptions/DomainException.cs
@@ -8,7 +8,18 @@ public class DomainException : Exception
 {
     public required string ErrorCode { get; init; }
 
+    public DomainException() : base()
+    {
+        ErrorCode = "DOMAIN_ERROR";
+    }
+
     public DomainException(string message) : base(message)
+    {
+        ErrorCode = "DOMAIN_ERROR";
+    }
+
+    public DomainException(string message, Exception innerException) 
+        : base(message, innerException)
     {
         ErrorCode = "DOMAIN_ERROR";
     }
@@ -18,13 +29,9 @@ public class DomainException : Exception
         ErrorCode = errorCode;
     }
 
-    public DomainException(string message, Exception innerException) : base(message, innerException)
-    {
-        ErrorCode = "DOMAIN_ERROR";
-    }
-
     [Obsolete("This API supports obsolete formatter-based serialization. See https://aka.ms/dotnet-warnings/SYSLIB0051")]
-    protected DomainException(SerializationInfo info, StreamingContext context) : base(info, context)
+    protected DomainException(SerializationInfo info, StreamingContext context) 
+        : base(info, context)
     {
         ErrorCode = info.GetString(nameof(ErrorCode)) ?? "DOMAIN_ERROR";
     }
