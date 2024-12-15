a8506abaa21e9b26b566a989c5b938ef56bd0842
Zubeid Hendricks <zubeid.hendricks@gmail.com>
2024-12-15 23:44:15 +0200

    Ensure migration classes are partial with unique Up and Down methods

--- src/IslamicPOS.Infrastructure/Data/Migrations/20240404001_InitialSchema.cs
+++ src/IslamicPOS.Infrastructure/Data/Migrations/20240404001_InitialSchema.cs
@@ -3,7 +3,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 
 namespace IslamicPOS.Infrastructure.Data.Migrations;
 
-public class InitialSchema : Migration
+public partial class InitialSchema : Migration
 {
     protected override void Up(MigrationBuilder migrationBuilder)
     {
--- src/IslamicPOS.Infrastructure/Data/Migrations/20241208001_InitialCreate.cs
+++ src/IslamicPOS.Infrastructure/Data/Migrations/20241208001_InitialCreate.cs
@@ -3,7 +3,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 
 namespace IslamicPOS.Infrastructure.Data.Migrations;
 
-public class InitialCreate : Migration
+public partial class InitialCreate : Migration
 {
     protected override void Up(MigrationBuilder migrationBuilder)
     {
