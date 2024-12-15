4bec287ab25d6b1bf7d6ac0dc1706d30cf961d2b
Zubeid Hendricks <zubeid.hendricks@gmail.com>
2024-12-15 20:59:44 +0200

    Resolve namespace and reference issues for Identity, Logistics, and Routing

--- src/IslamicPOS.Infrastructure/Data/Configurations/OptimizedRouteConfiguration.cs
+++ src/IslamicPOS.Infrastructure/Data/Configurations/OptimizedRouteConfiguration.cs
@@ -1,33 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using IslamicPOS.Domain.Logistics;
+
 namespace IslamicPOS.Infrastructure.Data.Configurations;
 
 public class OptimizedRouteConfiguration : IEntityTypeConfiguration<OptimizedRoute>
 {
     public void Configure(EntityTypeBuilder<OptimizedRoute> builder)
     {
-        builder.HasKey(e => e.Id);
-
-        builder.Property(e => e.Date)
-            .IsRequired();
-
-        builder.Property(e => e.Status)
-            .IsRequired()
-            .HasMaxLength(50);
-
-        builder.Property(e => e.TotalDistance)
-            .HasPrecision(10, 2);
-
-        builder.Property(e => e.EstimatedDuration)
-            .IsRequired();
-
-        builder.HasOne(e => e.Vehicle)
-            .WithMany()
-            .HasForeignKey(e => e.VehicleId)
-            .OnDelete(DeleteBehavior.Restrict);
-
-        builder.HasMany(e => e.Stops)
-            .WithMany()
-            .UsingEntity("OptimizedRouteStops",
-                l => l.HasOne(typeof(DeliveryPoint)).WithMany().HasForeignKey("DeliveryPointId"),
-                r => r.HasOne(typeof(OptimizedRoute)).WithMany().HasForeignKey("OptimizedRouteId"));
+        builder.HasKey(r => r.Id);
+        builder.Property(r => r.RouteId).IsRequired();
+        builder.HasOne<Vehicle>().WithMany();
+        builder.Property(r => r.TotalDistance).HasPrecision(18, 2);
     }
 }
