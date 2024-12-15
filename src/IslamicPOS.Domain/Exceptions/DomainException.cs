3d914229ab6a74ec84f8b18f96f678772795aabf
ZubeidHendricks <zubeid.hendricks@gmail.com>
2024-12-16 00:31:17 +0200

    mcp:DEVX: Fix DomainException warnings
    - Add required modifier to ErrorCode property
    - Add Obsolete attributes to serialization members
    - Initialize ErrorCode in all constructors
    - Fix nullable warnings

--- src/IslamicPOS.Domain/Exceptions/DomainException.cs
+++ src/IslamicPOS.Domain/Exceptions/DomainException.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Runtime.Serialization;
 
 namespace IslamicPOS.Domain.Exceptions;
@@ -5,23 +6,30 @@ namespace IslamicPOS.Domain.Exceptions;
 [Serializable]
 public class DomainException : Exception
 {
-    public string ErrorCode { get; }
+    public required string ErrorCode { get; init; }
 
-    public DomainException() : base() { }
-
-    public DomainException(string message) : base(message) { }
-
-    public DomainException(string message, Exception innerException) 
-        : base(message, innerException) { }
+    public DomainException(string message) : base(message)
+    {
+        ErrorCode = "DOMAIN_ERROR";
+    }
 
     public DomainException(string message, string errorCode) : base(message)
     {
         ErrorCode = errorCode;
     }
 
-    protected DomainException(SerializationInfo info, StreamingContext context) 
-        : base(info, context) { }
+    public DomainException(string message, Exception innerException) : base(message, innerException)
+    {
+        ErrorCode = "DOMAIN_ERROR";
+    }
 
+    [Obsolete("This API supports obsolete formatter-based serialization. See https://aka.ms/dotnet-warnings/SYSLIB0051")]
+    protected DomainException(SerializationInfo info, StreamingContext context) : base(info, context)
+    {
+        ErrorCode = info.GetString(nameof(ErrorCode)) ?? "DOMAIN_ERROR";
+    }
+
+    [Obsolete("This API supports obsolete formatter-based serialization. See https://aka.ms/dotnet-warnings/SYSLIB0051")]
     public override void GetObjectData(SerializationInfo info, StreamingContext context)
     {
         base.GetObjectData(info, context);
