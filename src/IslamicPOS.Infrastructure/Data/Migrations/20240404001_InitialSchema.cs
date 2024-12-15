205b0e7d081556b61d119c6d80ea3f9ca2212af7
Zubeid Hendricks <zubeid.hendricks@gmail.com>
2024-12-16 00:06:10 +0200

    Simplify migrations to remove partial method complexity

--- src/IslamicPOS.Infrastructure/Data/Migrations/20240404001_InitialSchema.cs
+++ src/IslamicPOS.Infrastructure/Data/Migrations/20240404001_InitialSchema.cs
@@ -3,7 +3,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 
 namespace IslamicPOS.Infrastructure.Data.Migrations;
 
-public class InitialSchema_Generated : Migration
+public partial class InitialSchema : Migration
 {
     protected override void Up(MigrationBuilder migrationBuilder)
     {
